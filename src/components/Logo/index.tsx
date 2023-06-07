5865310e2c3058c4886624412b85341df5a35833
cheeselemon <cheeselemon.yarn@gmail.com>
2023-06-07 11:11:43 +0900

    feat: remove BaseComponents

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,19 @@
+/**
+ * 탈잉 Biz 로고 컴포넌트
+ */
+const LogoComponent = () => {
+  return (
+    <div
+      className="h-6 w-12 sm:h-6 sm:w-12"
+      style={{
+        backgroundImage:
+          "url(https://front-img.taling.me/Content/app3/img/logo/img-logo-black-66.png)",
+        backgroundSize: "contain",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    ></div>
+  );
+};
+
+export default LogoComponent;
