3d3226e6ec62d88cdc864eb3caf87c6a55005c22
cheeselemon <cheeselemon.yarn@gmail.com>
2023-06-07 15:48:35 +0900

    feat: DevMarker 닫기 기능

--- src/components/DevMarker/index.tsx
+++ src/components/DevMarker/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 const DevMarker = () => {
+  const [isShowing, setIsShowing] = useState<boolean>(false);
   const [isDev, setIsDev] = useState<boolean>(false);
   const [devMsag, setDevMsg] = useState<string>("개발/테스트 모드에용");
 
@@ -10,13 +11,22 @@ const DevMarker = () => {
     const domain = window.location.hostname;
     if (domain.match(new RegExp(devDomains.join("|")))) {
       setIsDev(true);
+      setIsShowing(true);
     }
   }, []);
 
-  if (isDev) {
+  if (isDev && isShowing) {
     return (
       <div className="flex h-6 w-full items-center bg-taling-pink-50 border-b-2 border-b-taling-pink-400">
         <div className="mx-auto text-xs">🤖 {devMsag}</div>
+        <div
+          className="text-xs px-8"
+          onClick={() => {
+            setIsShowing(false);
+          }}
+        >
+          ❌
+        </div>
       </div>
     );
   } else {
