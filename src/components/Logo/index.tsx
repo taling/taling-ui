7d359e72e95d6cb47c94b815feb2212d2ec0f959
cheeselemon <cheeselemon.yarn@gmail.com>
2023-06-09 14:50:42 +0900

    feat: sized LogoComponent

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,10 +1,15 @@
 /**
  * 탈잉 Biz 로고 컴포넌트
  */
-const LogoComponent = () => {
+const LogoComponent = ({ size = "sm" }: { size?: "sm" | "md" | "lg" }) => {
+  const sizeMap = {
+    sm: "h-7 w-12 sm:h-7 sm:w-12",
+    md: "h-10 w-16 sm:h-10 sm:w-16",
+    lg: "h-12 w-20 sm:h-12 sm:w-20",
+  };
   return (
     <div
-      className="h-7 w-12 sm:h-7 sm:w-12"
+      className={sizeMap[size]}
       style={{
         backgroundImage:
           "url(https://front-img.taling.me/Content/app3/img/logo/img-logo-black-66.png)",
