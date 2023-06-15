daaf39ea005a238103130b3860dee00a8adac584
cheeselemon <cheeselemon.yarn@gmail.com>
2023-06-15 17:27:13 +0900

    feat: PrimaryButton 맵시 수정

--- src/components/Buttons/PrimaryButton.tsx
+++ src/components/Buttons/PrimaryButton.tsx
@@ -1,7 +1,7 @@
-const PrimaryButton = ({ children }: { children: any }): JSX.Element => {
+const PrimaryButton = (props: any): JSX.Element => {
   return (
-    <div className="bg-taling-pink-400 text-white px-2 py-1 rounded-md drop-shadow-sm cursor-pointer">
-      {children}
+    <div className="bg-taling-pink-400 text-white text-center px-2 py-1.5 rounded-md drop-shadow-sm cursor-pointer">
+      {props.children}
     </div>
   );
 };
