21c139b85ebffe68d7b52ef5961773ee5fd8aa9c
cheeselemon <cheeselemon.yarn@gmail.com>
2023-07-04 14:31:00 +0900

    feat: PrimaryButton의 loadingState 애니메이션

--- src/components/Buttons/PrimaryButton.tsx
+++ src/components/Buttons/PrimaryButton.tsx
@@ -1,18 +1,20 @@
 const PrimaryButton = ({
   children,
   size = "md",
+  showLoadingState = false,
 }: {
   children: React.ReactNode;
   size?: "sm" | "md" | "lg";
+  showLoadingState?: boolean;
 }): JSX.Element => {
   const pad = function () {
     switch (size) {
       case "sm":
-        return "px-2 py-1.5";
+        return "px-2 py-1.5 h-8";
       case "md":
-        return "px-4 py-2";
+        return "px-4 py-2 h-10";
       case "lg":
-        return "px-6 py-3";
+        return "px-6 py-3 h-12";
     }
   };
   return (
@@ -22,7 +24,15 @@ const PrimaryButton = ({
         pad()
       }
     >
-      {children}
+      {showLoadingState ? (
+        <div className="flex w-full h-full justify-center items-center gap-2">
+          <div className="w-1.5 h-1.5 bg-white rounded-full animate-ping"></div>
+          <div className="w-1.5 h-1.5 bg-white rounded-full animate-ping transition-transform delay-300"></div>
+          <div className="w-1.5 h-1.5 bg-white rounded-full animate-ping transition-transform delay-600"></div>
+        </div>
+      ) : (
+        children
+      )}
     </div>
   );
 };
