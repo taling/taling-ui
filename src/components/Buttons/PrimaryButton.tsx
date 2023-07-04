5732232b2f57885f592b9dd7273a2df9125a2bf2
cheeselemon <cheeselemon.yarn@gmail.com>
2023-07-04 09:50:50 +0900

    feat: sized PrimaryButton

--- src/components/Buttons/PrimaryButton.tsx
+++ src/components/Buttons/PrimaryButton.tsx
@@ -1,7 +1,28 @@
-const PrimaryButton = (props: any): JSX.Element => {
+const PrimaryButton = ({
+  children,
+  size = "md",
+}: {
+  children: React.ReactNode;
+  size?: "sm" | "md" | "lg";
+}): JSX.Element => {
+  const pad = function () {
+    switch (size) {
+      case "sm":
+        return "px-2 py-1.5";
+      case "md":
+        return "px-4 py-2";
+      case "lg":
+        return "px-6 py-3";
+    }
+  };
   return (
-    <div className="bg-taling-pink-400 text-white text-center px-2 py-1.5 rounded-md drop-shadow-sm cursor-pointer">
-      {props.children}
+    <div
+      className={
+        "bg-taling-pink-400 text-white text-center px-2 py-1.5 rounded-md drop-shadow-sm cursor-pointer" +
+        pad()
+      }
+    >
+      {children}
     </div>
   );
 };
