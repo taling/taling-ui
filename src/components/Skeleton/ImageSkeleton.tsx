1de7464d258ed450d468aae5be3099dbee28ffbd
cheeselemon <cheeselemon.yarn@gmail.com>
2023-07-19 14:10:53 +0900

    feat: 이미지 스켈레톤 컴포넌트

---
+++ src/components/Skeleton/ImageSkeleton.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable @next/next/no-img-element */
+export function ImageSkeletonComponent({
+  isLoading = true,
+  className,
+  imageUrl,
+  imageAlt,
+  placeholder,
+}: {
+  isLoading: boolean;
+  className?: any;
+  imageUrl?: string;
+  imageAlt?: string;
+  placeholder: any;
+}) {
+  if (isLoading) {
+    return <div className={...className}>{placeholder}</div>;
+  }
+  return (
+    <div className={...className}>
+      {imageUrl ? <img src={imageUrl} alt={imageAlt} /> : placeholder}
+    </div>
+  );
+}
