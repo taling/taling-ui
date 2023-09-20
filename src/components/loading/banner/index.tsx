2e672e2ec7948f77f1ccbb70daa77a054d991dd1
cheeselemon <cheeselemon.yarn@gmail.com>
2023-09-20 17:09:33 +0900

    feat: loading-banner 컴포넌트 추가

---
+++ src/components/loading/banner/index.tsx
@@ -0,0 +1,12 @@
+import LoadingSpinner from "../spinner";
+
+export default function LoadingBannerComponent({ title }: { title: string }) {
+  return (
+    <div className="mx-auto max-w-sm py-12 text-center min-h-screen mt-44">
+      {title}
+      <div className="mx-auto w-6 text-taling-pink-400 p-4">
+        <LoadingSpinner className="w-5 h-5" />
+      </div>
+    </div>
+  );
+}
