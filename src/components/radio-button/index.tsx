050698ae5beae3c0173fe2d9af8e5908a22aed1a
harang-taling <harang@taling.me>
2023-10-05 17:55:12 +0900

    feat - 라디오박스 ui 수정 *체크 표시 -> 라디오

--- src/components/radio-button/index.tsx
+++ src/components/radio-button/index.tsx
@@ -16,11 +16,10 @@ export default function RadioButton({
       <label>
         <input
           type="radio"
-          className="form-radio h-5 w-5 text-taling-pink-400 focus:ring-2 focus:ring-red-500 focus:outline-none"
+          className="form-radio h-5 w-5 text-taling-pink-400 focus:ring-1 focus:ring-red-500 focus:outline-none"
           checked={checked}
           onChange={toggleChecked}
         />
-
         <span className="ml-2">{label}</span>
       </label>
     </div>
