bcd11a382a62821bb68089cb04e1ec44cfcf365a
cheeselemon <cheeselemon.yarn@gmail.com>
2023-10-17 17:55:27 +0900

    feat: SelectBox 로직 업데이트

--- src/components/select-box/index.tsx
+++ src/components/select-box/index.tsx
@@ -23,36 +23,37 @@ export default function SelectBox({
   const [selected, setSelected] = useState<ISelectBoxItem | null>(null);
 
   /**
-   * defaultSelection이 변경되었을때, 셀렉트박스를 선택해줍니다.
-   * 기존 선택값과 같거나, 이미 선택된 경우에는 무시
-   */
-  const onSelectionPropChanged = useCallback(() => {
-    if (selected === defaultSelection || selected) return;
-    setSelected(defaultSelection);
-  }, [defaultSelection, selected, setSelected]);
-
-  /**
    * 리스트가 업데이트 되고, 이미 선택된 값이 리스트에 없으면 선택을 초기화 합니다.
    */
   const onListChanged = useCallback(
     (newList: ISelectBoxItem[]) => {
       if (newList === _internaList) return;
       setInternalList(newList);
-      if (!newList.find((item) => item?.id === selected?.id)) {
+      if (
+        !newList.find(
+          (item) =>
+            item?.id === selected?.id || item?.id === defaultSelection?.id,
+        )
+      ) {
         setSelected(null);
+        return;
+      }
+
+      if (!selected) {
+        setSelected(defaultSelection);
       }
     },
-    [_internaList, selected],
+    [_internaList, defaultSelection, selected],
   );
 
   useEffect(() => {
+    console.log(`onSelected`, selected?.name);
     onSelected(selected);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selected]);
 
   useEffect(() => {
     onListChanged(list);
-    onSelectionPropChanged();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [defaultSelection, list]);
 
@@ -60,7 +61,7 @@ export default function SelectBox({
     <Listbox value={selected} onChange={setSelected}>
       {({ open }) => (
         <>
-          <div className="relative mt-2">
+          <div className="relative">
             <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-taling-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-taling-pink-400 sm:text-sm sm:leading-6">
               <span className="block truncate">
                 {selected?.name ?? "선택해주세요"}
@@ -80,44 +81,54 @@ export default function SelectBox({
               leaveTo="opacity-0"
             >
               <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {list.map((item) => (
-                  <Listbox.Option
-                    key={item.id}
-                    className={({ active }) =>
-                      classNames(
-                        active
-                          ? "bg-taling-pink-400 text-white"
-                          : "text-taling-gray-900",
-                        "relative cursor-default select-none py-2 pl-3 pr-9",
-                      )
-                    }
-                    value={item}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <span
-                          className={classNames(
-                            selected ? "font-semibold" : "font-normal",
-                            "block truncate",
-                          )}
-                        >
-                          {item.name}
-                        </span>
-
-                        {selected ? (
+                {list.map((item) => {
+                  const internalSelected = selected?.id === item.id;
+                  return (
+                    <Listbox.Option
+                      key={item.id}
+                      className={({ active }) =>
+                        classNames(
+                          active || internalSelected
+                            ? "bg-taling-pink-400 text-white"
+                            : "text-taling-gray-900",
+                          "relative cursor-default select-none py-2 pl-3 pr-9",
+                        )
+                      }
+                      value={item}
+                    >
+                      {({ selected, active }) => (
+                        <>
                           <span
                             className={classNames(
-                              active ? "text-white" : "text-taling-pink-400",
-                              "absolute inset-y-0 right-0 flex items-center pr-4",
+                              selected || internalSelected
+                                ? "font-semibold"
+                                : "font-normal",
+                              "block truncate",
                             )}
                           >
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                            {item.name}
                           </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
+
+                          {selected || internalSelected ? (
+                            <span
+                              className={classNames(
+                                active || internalSelected
+                                  ? "text-white"
+                                  : "text-taling-pink-400",
+                                "absolute inset-y-0 right-0 flex items-center pr-4",
+                              )}
+                            >
+                              <CheckIcon
+                                className="h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Listbox.Option>
+                  );
+                })}
               </Listbox.Options>
             </Transition>
           </div>
