e5e08bc5b8a5714ed43cdfe5f0ee45fe6fa2c81e
code-xhyun <shhong@taling.me>
2023-10-18 20:10:02 +0900

    feat: overflow mode 추가

--- src/components/skeleton/text/index.tsx
+++ src/components/skeleton/text/index.tsx
@@ -3,19 +3,23 @@ export function TextSkeletonComponent({
   expectedWidth,
   className,
   text,
+  isOverflowMode,
 }: {
   isLoading: boolean;
   expectedWidth?: string;
   className?: string;
   text?: string;
+  isOverflowMode?: boolean;
 }) {
   const loadingClass = isLoading
     ? "animate-pulse bg-taling-gray-100 text-transparent rounded-lg"
     : "";
 
+  const overflowClass = isOverflowMode ? "overflow-y-scroll " : "";
+
   return (
     <p
-      className={`${loadingClass} ${className ?? ""}`}
+      className={`${loadingClass} ${overflowClass} ${className ?? ""}`}
       style={{
         width: isLoading ? expectedWidth : "auto",
       }}
