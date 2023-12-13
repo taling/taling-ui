aed05bfa08063fbf6d8d1190ba4f89403add01f6
code-xhyun <shhong@taling.me>
2023-12-13 12:55:44 +0900

    fix: 버튼 사이즈 재조정

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,9 +18,10 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
       },
       size: {
-        default: "h-12 px-6 py-3  text-base",
-        sm: "h-9 px-4 py-2 text-xs",
-        lg: "h-14 px-8 py-4 text-lg" ,
+        default: "h-9 px-4 py-2",
+        sm: "h-8 rounded-md px-3 text-xs",
+        lg: "h-10 rounded-md px-8",
+        xl: "h-12 rounded-md px-8 ",
       },
     },
     defaultVariants: {
