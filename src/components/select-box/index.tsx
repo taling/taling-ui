b04d8afb91542327f38c938d9052bcda605d6177
code-xhyun <shhong@taling.me>
2023-12-15 21:31:33 +0900

    fix: 셀렉트박스 스크롤바 삭제

--- src/components/select-box/index.tsx
+++ src/components/select-box/index.tsx
@@ -108,7 +108,7 @@ export default function SelectBox({
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              <Listbox.Options className="no-scrollbar absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                 {list.map((item) => {
                   const internalSelected = selected?.id === item.id;
                   return (
