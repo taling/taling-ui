6124d71d047c4afd519c22563900c3cb07cf219b
cheeselemon <cheeselemon.yarn@gmail.com>
2023-12-21 19:12:50 +0900

    chore: AppCookieConstants 코멘트 업데이트

--- src/constants/app-cookie/index.ts
+++ src/constants/app-cookie/index.ts
@@ -1,9 +1,15 @@
 const AppCookieConstants = {
   isApp: "taling-app", // true / false
   appVersion: "taling-app-version", // eg. 4.0.40
+
   appBuildNum: "taling-app-build-num", // eg. 436
-  appPlatform: "taling-app-platform", // iOS / Android
+  
+  // iOS: "iOS" on newer iOS devices "iPhone OS" on older devices (including older iPad models), "iPadOS" for iPads using iPadOS 15.0 or higher.
+  // Android: "Android"
+  appPlatform: "taling-app-platform",
+
   appUserAuth: "taling-app-user-auth",
+
   crossDoaminUserAuth: "taling-crossdomain-user-auth",
 };
 
