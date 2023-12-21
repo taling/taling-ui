a6997973b17a4ba104c3abe3ca2960681ae9930d
cheeselemon <cheeselemon.yarn@gmail.com>
2023-12-21 19:09:25 +0900

    feat: AppCookieConstants 업데이트

--- src/constants/app-cookie/index.ts
+++ src/constants/app-cookie/index.ts
@@ -1,6 +1,8 @@
 const AppCookieConstants = {
-  isApp: "taling-app",
-  appVersion: "taling-app-version",
+  isApp: "taling-app", // true / false
+  appVersion: "taling-app-version", // eg. 4.0.40
+  appBuildNum: "taling-app-build-num", // eg. 436
+  appPlatform: "taling-app-platform", // iOS / Android
   appUserAuth: "taling-app-user-auth",
   crossDoaminUserAuth: "taling-crossdomain-user-auth",
 };
