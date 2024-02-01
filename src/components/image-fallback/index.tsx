1661bba3e08911894fe34d85e4158a38f7921b4d
OneUne <yunhee.won.swe@gmail.com>
2024-02-01 19:31:21 +0900

    fix: [TONE-543] ImageFallback component에서 src가 없을 때도 fallback시키기 && defaultImage profile-default-thumb말고 default로 바꿈

--- src/components/image-fallback/index.tsx
+++ src/components/image-fallback/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { useState } from "react";
-import Image from 'next/image'
+import Image from "next/image";
 /**
  * 이미지 로드에 실패하면 fallback 컴포넌트를 표시합니다.
  * @param param0
@@ -13,63 +13,64 @@ export default function ImageFallback({
   fallback,
   loading = "lazy",
   nextImageOption,
-  useDefaultImg
+  useDefaultImg,
 }: {
   src: string;
   alt: string;
   className: string;
   fallback: React.ReactNode | string;
   loading?: "eager" | "lazy" | undefined;
-  nextImageOption?:{
-    width:number;
-    height:number;
-  }
-  useDefaultImg?:boolean
+  nextImageOption?: {
+    width: number;
+    height: number;
+  };
+  useDefaultImg?: boolean;
 }) {
   const [showFallback, setShowFallback] = useState<boolean>(false);
 
-  if (showFallback) {
-    if(useDefaultImg){
-      const defaultSrc = "//img.taling.me/Content/Images/placeholders/profile-default.thumb.jpg"
-      return(
-        nextImageOption ? <Image
+  if (showFallback || !src) {
+    if (useDefaultImg) {
+      const defaultSrc =
+        "//img.taling.me/Content/Images/placeholders/profile-default.jpg";
+      return nextImageOption ? (
+        <Image
           src={defaultSrc}
           alt={alt}
           className={className}
           width={nextImageOption.width}
           height={nextImageOption.height}
           loading={loading}
-      /> :
+        />
+      ) : (
         <img
           src={defaultSrc}
           alt={alt}
           className={className}
           loading={loading}
         />
-      )
+      );
     }
     return <div className={className}>{fallback}</div>;
   }
 
-  const protocolCheck = (src:string) => {
-    if (src.startsWith('//')) {
-      return 'https:' + src;
+  const protocolCheck = (src: string) => {
+    if (src.startsWith("//")) {
+      return "https:" + src;
     }
-    if (!src.startsWith('http://') && !src.startsWith('https://')) {
-      return 'https://' + src;
+    if (!src.startsWith("http://") && !src.startsWith("https://")) {
+      return "https://" + src;
     }
     return src;
-  }
+  };
 
-  const domainCheck = (src:string) => {
+  const domainCheck = (src: string) => {
     if (!src) return false;
-    if (src.includes('taling.me')) {
+    if (src.includes("taling.me")) {
       return true;
     }
-  }
+  };
 
-  return (
-    nextImageOption && (domainCheck(src))?
+  return nextImageOption && domainCheck(src) ? (
     <Image
       src={protocolCheck(src)}
       alt={alt}
@@ -80,7 +81,8 @@ export default function ImageFallback({
       onError={() => {
         setShowFallback(true);
       }}
-    /> :
+    />
+  ) : (
     <img
       src={src}
       alt={alt}
