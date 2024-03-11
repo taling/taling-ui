886b55c7fcd73c1c07b6097d160f19dde1cdb282
OneUne <yunhee.won.swe@gmail.com>
2024-03-11 17:53:46 +0900

    Facebook 로고 동그란 배경 없애고 f만 있는 걸로 수정

--- src/components/icons/facebook/index.tsx
+++ src/components/icons/facebook/index.tsx
@@ -2,17 +2,8 @@ import { classNames } from "@taling-ui/util/tailwind-util/class-names";
 
 export default function FacebookIcon(props: any) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className={classNames(props.className)} viewBox="0 0 20 20">
-      <g id="Symbols" stroke="none" stroke-width="1" fill="currentColor" fill-rule="evenodd">
-          <g id="footer/idle" transform="translate(-120.000000, -130.000000)">
-              <rect fill="#F5F6F6" x="0" y="0" width="1440" height="522"/>
-              <g id="Icon/facebook" transform="translate(120.000000, 130.000000)" fill="currentColor">
-                  <g id="footer-facebook-(1)">
-                      <path d="M20,10.0611134 C20,4.50450981 15.5228516,0 10,0 C4.47714844,0 0,4.50450981 0,10.0611134 C0,15.0829059 3.65685547,19.24522 8.4375,20 L8.4375,12.969404 L5.89843749,12.969404 L5.89843749,10.0611134 L8.4375,10.0611134 L8.4375,7.84452436 C8.4375,5.32295781 9.93042968,3.93012242 12.2146484,3.93012242 C13.3087304,3.93012242 14.4531251,4.12662855 14.4531251,4.12662855 L14.4531251,6.60260567 L13.1921484,6.60260567 C11.9499023,6.60260567 11.5625,7.37815638 11.5625,8.17380966 L11.5625,10.0611134 L14.3359375,10.0611134 L13.8925781,12.969404 L11.5625,12.969404 L11.5625,20 C16.3431445,19.24522 20,15.0829059 20,10.0611134" id="Facebook" fill-rule="nonzero"/>
-                  </g>
-              </g>
-          </g>
-      </g>
+    <svg xmlns="http://www.w3.org/2000/svg"className={classNames(props.className)} viewBox="0 0 20 20" fill="currentColor">
+      <path d="M16.94 12H13.77V23.5H9V12H6.625V8.03497H9V5.65503C9 2.42503 10.34 0.5 14.15 0.5H17.32V4.46503H15.32C13.82 4.46503 13.74 5.01999 13.74 6.04999V8.04999H17.33L16.94 12Z"></path>
     </svg>
   );
 }
