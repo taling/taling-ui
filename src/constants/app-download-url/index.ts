3560305ee6523e27755264dc8f7667165a71b735
harang-taling <harang@taling.me>
2024-03-27 12:01:47 +0900

    feat: 앱 스토어 주소 관리 추가

---
+++ src/constants/app-download-url/index.ts
@@ -0,0 +1,6 @@
+// 앱 스토어 주소 관리.
+
+export const APP_DOWNLOAD_URLS = {
+  Android: "https://play.google.com/store/apps/details?id=com.taling",
+  iOS: "https://apps.apple.com/kr/app/id1519711837",
+};
