e2b60b85a209ca2e8cf34e68451321d6855fa36f
harang-taling <harang@taling.me>
2024-03-27 14:33:55 +0900

    fix: 잘못된 앱 스토어 주소 변경

--- src/constants/app-download-url/index.ts
+++ src/constants/app-download-url/index.ts
@@ -2,5 +2,5 @@
 
 export const APP_DOWNLOAD_URLS = {
   Android: "https://play.google.com/store/apps/details?id=com.taling",
-  iOS: "https://apps.apple.com/kr/app/id1519711837",
+  iOS: "https://apps.apple.com/kr/app/%ED%83%88%EC%9E%89-%EB%B0%B0%EC%9B%80%EC%9D%84-%EC%9E%AC%EB%B0%8C%EA%B2%8C/id1153218962",
 };
