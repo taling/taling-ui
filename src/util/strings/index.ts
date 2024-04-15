0c6617cb679c135e294251943b1606fb355ada9e
cheeselemon <cheeselemon.yarn@gmail.com>
2024-04-15 11:21:13 +0900

    feat: [TONE-760] kspay 불가 문자열 필터링 함수 추가

--- src/util/strings/index.ts
+++ src/util/strings/index.ts
@@ -1,3 +1,12 @@
 export function kebabCaseToCamelCase(input: string): string {
   return input.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
 }
+
+/**
+ * Kspay 불가능 문자 필터링
+ * @param input
+ * @returns
+ */
+export function filterKspayForbiddenChar(input: string): string {
+  return input.replace(/["'`~]/g, "");
+}
