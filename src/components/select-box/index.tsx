1425762f5d169d5c5a40893f244ace201a51f97f
Kyusik Jeong <starplanter93@gmail.com>
2024-10-24 16:40:09 +0900

    fix: [TONE-1117] selectbox의 return이 undefined가 안되도록 수정

--- src/components/select-box/index.tsx
+++ src/components/select-box/index.tsx
@@ -33,8 +33,7 @@ export default function SelectBox({
     (newList: ISelectBoxItem[]) => {
       // 먼저 선택상황을 편집하고,
       const found = newList.find(
-        (item) =>
-          item?.id === selected?.id || item?.id === defaultSelection?.id,
+        (item) => item?.id === selected?.id || item?.id === defaultSelection?.id
       );
 
       if (!found) {
@@ -50,7 +49,7 @@ export default function SelectBox({
       if (newList === _internaList) return;
       setInternalList(newList);
     },
-    [_internaList, defaultSelection, selected],
+    [_internaList, defaultSelection, selected]
   );
 
   // useEffect(() => {
@@ -72,8 +71,8 @@ export default function SelectBox({
     setSelected(defaultSelection);
   }, [defaultSelection]);
 
-  if (list.length === 0 && defaultSelection === null) return;
-  if (!_hydrated) return;
+  if (list.length === 0 && defaultSelection === null) return <></>;
+  if (!_hydrated) return <></>;
 
   return (
     <Listbox
@@ -91,15 +90,15 @@ export default function SelectBox({
                 "relative w-full cursor-pointer rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-taling-gray-900 shadow-sm ring-1 ring-inset ring-gray-300  sm:text-sm sm:leading-6",
                 enabled
                   ? "focus:outline-none focus:ring-2 focus:ring-taling-pink "
-                  : "!bg-taling-gray-300 !cursor-not-allowed !text-taling-gray-800 opacity-50 ",
+                  : "!cursor-not-allowed !bg-taling-gray-300 !text-taling-gray-800 opacity-50 "
               )}
             >
               <span className="block truncate">
                 {selected?.name ?? "선택해 주세요"}
               </span>
-              <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                 <ChevronUpDownIcon
-                  className="w-5 h-5 text-taling-gray-400"
+                  className="h-5 w-5 text-taling-gray-400"
                   aria-hidden="true"
                 />
               </span>
@@ -111,7 +110,7 @@ export default function SelectBox({
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Listbox.Options className="absolute z-10 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg no-scrollbar max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              <Listbox.Options className="no-scrollbar absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                 {list.map((item) => {
                   const internalSelected = selected?.id === item.id;
                   const excludeToday = isToday && !item.display;
@@ -124,7 +123,7 @@ export default function SelectBox({
                           active || internalSelected
                             ? "bg-taling-gray-100"
                             : "text-taling-gray-900",
-                          "relative cursor-pointer select-none py-2 pl-3 pr-9",
+                          "relative cursor-pointer select-none py-2 pl-3 pr-9"
                         )
                       }
                       value={item}
@@ -136,7 +135,7 @@ export default function SelectBox({
                               selected || internalSelected
                                 ? "font-semibold"
                                 : "font-normal",
-                              "block truncate",
+                              "block truncate"
                             )}
                           >
                             {item.name}
@@ -148,11 +147,11 @@ export default function SelectBox({
                                 active || internalSelected
                                   ? "text-taling-black"
                                   : "text-taling-pink",
-                                "absolute inset-y-0 right-0 flex items-center pr-4",
+                                "absolute inset-y-0 right-0 flex items-center pr-4"
                               )}
                             >
                               <CheckIcon
-                                className="w-5 h-5"
+                                className="h-5 w-5"
                                 aria-hidden="true"
                               />
                             </span>
