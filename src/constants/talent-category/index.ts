1c5ab9d3371cee995262ded879479a86724d7f3c
Yunhee Won <yunhee.won.swe@gmail.com>
2024-11-12 20:29:16 +0900

    feat: [TONE-1145] TalentSpecialRegion enum에 OFFLINE 값 추가

--- src/constants/talent-category/index.ts
+++ src/constants/talent-category/index.ts
@@ -9,4 +9,5 @@ export enum TalentCategory {
 
 export enum TalentSpecialRegion {
   ONLINE = 64, // 온라인 라이브의 경우 region = 64의 특수값을 가짐
+  OFFLINE = 65, // 무료체험 나오면서 생긴 값으로, 이 값일 경우 accountAddress의 주소 활용
 }
