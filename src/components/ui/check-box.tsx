515bef07b6d3d845c1e5487ddc13640fe4dc4ecc
Kyusik Jeong <starplanter93@gmail.com>
2024-11-21 18:07:18 +0900

    feat: [TONE-1205] checkBox 컴포넌트 추가

---
+++ src/components/ui/check-box.tsx
@@ -0,0 +1,28 @@
+import { classNames } from "@taling-ui/util/tailwind-util/class-names";
+
+const sizeMap = {
+  normal: "w-5 h-5",
+  small: "w-4 h-4",
+};
+type sizeMapType = keyof typeof sizeMap;
+
+interface CheckBoxProps {
+  size?: sizeMapType;
+}
+
+export default function CheckBox({ size = "normal" }: CheckBoxProps) {
+  return (
+    <input
+      className={classNames(
+        sizeMap[size],
+        "rounded-md border-1 border-taling-gray-400 text-primary",
+        "hover:ring-2 hover:ring-offset-0 hover:ring-primary/10",
+        "focus-visible:ring-2 focus-visible:ring-offset-0 focus-visible:ring-primary/15",
+        "focus:ring-2 focus:ring-offset-0 focus:ring-primary/20",
+        "checked:focus:ring-0 checked:focus:ring-offset-0",
+        "appearance-none",
+      )}
+      type="checkbox"
+    />
+  );
+}
