1b32335fc7a29aac754f80b4b938834a64235062
Kyusik Jeong <starplanter93@gmail.com>
2024-11-22 11:54:22 +0900

    feat: [TONE-1184] 버튼 컴포넌트 size, icon, solidPrimary까지.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,11 +4,41 @@ import * as React from "react";
 
 import { cn } from "@taling-ui/util/tailwind-util/utils";
 
+const iconContainerVariants = cva("inline-flex items-center justify-center", {
+  variants: {
+    size: {
+      xs: "h-4 w-4",
+      sm: "h-4 w-4",
+      md: "h-6 w-6",
+      lg: "h-6 w-6",
+      // deprecated - default는 추후 사라질 예정.
+      default: "h-6 w-6",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+  },
+});
+
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  [
+    "inline-flex items-center justify-center gap-1 whitespace-nowrap rounded-lg transition-colors",
+    "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring",
+    "disabled:pointer-events-none",
+  ],
   {
     variants: {
       variant: {
+        solidPrimary: [
+          "bg-primary text-taling-white [&]:text-taling-white",
+          "disabled:bg-taling-gray-400 disabled:text-interaction-disabled/50",
+          "hover:bg-taling-pink-600",
+          "active:bg-taling-pink-700",
+        ],
+        solidSecondary: "",
+        oulinedPrimary: "",
+        outlinedSecondary: "",
+        // deprecated - 추후 사라질 속성
         default:
           "bg-taling-pink text-taling-white shadow hover:bg-taling-pink/80 disabled:bg-taling-gray-400",
         outline:
@@ -19,10 +49,12 @@ const buttonVariants = cva(
         underline: "underline hover:text-taling-gray-900 text-taling-gray-600 ",
       },
       size: {
+        xs: "h-7 min-w-[3rem] rounded-lg px-2.5 py-1.5 text-caption1-semibold",
+        sm: "h-[2.125rem] min-w-[3.5rem] rounded-lg px-3 py-2 text-label2-semibold",
+        md: "h-10 min-w-[4.25rem] rounded-lg px-3.5 py-2 text-body2normal-semibold",
+        lg: "h-12 min-w-[5.25rem] rounded-lg px-4 py-3 text-body2normal-semibold",
+        // deprecated - default는 추후 사라질 예정.
         default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        xl: "h-12 rounded-md px-8 ",
       },
     },
     defaultVariants: {
@@ -36,17 +68,39 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      leftIcon,
+      rightIcon,
+      children,
+      ...props
+    },
+    ref,
+  ) => {
     const Comp = asChild ? (Slot as any) : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        {leftIcon && (
+          <div className={cn(iconContainerVariants({ size }))}>{leftIcon}</div>
+        )}
+        {children}
+        {rightIcon && (
+          <div className={cn(iconContainerVariants({ size }))}>{rightIcon}</div>
+        )}
+      </Comp>
     );
   },
 );
