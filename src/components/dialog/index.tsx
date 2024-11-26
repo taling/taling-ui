6d6d05d3cb422baaf81e4b245a7c3be308a09cd6
Kyusik Jeong <starplanter93@gmail.com>
2024-11-26 10:51:23 +0900

    refactor: [TONE-1206] 다이알로그 버튼묶음 컴포넌트화

--- src/components/dialog/index.tsx
+++ src/components/dialog/index.tsx
@@ -6,7 +6,6 @@ import {
 import WaitingButton from "@taling-ui/buttons/waiting-button";
 import LoadingSpinner from "@taling-ui/loading/spinner";
 import { Button } from "@taling-ui/ui/button";
-
 interface DialogProps {
   title?: string;
   subTitle?: string;
@@ -55,95 +54,120 @@ export default function Dialog({
           </DialogTitle>
           <section>{children}</section>
         </div>
-        <div className="flex gap-2">
-          {!onPromiseCancel && (
-            <Button
-              size={"md"}
-              variant={"outlinedSecondary"}
-              className="w-full"
-              onClick={() => {
-                onCancel?.();
-                setIsOpen?.(false);
-              }}
-            >
-              {cancelLabel}
-            </Button>
-          )}
-          {onPromiseCancel && (
-            <WaitingButton
-              className="w-full"
-              render={{
-                normal: (
-                  <Button
-                    size={"md"}
-                    variant={"outlinedSecondary"}
-                    className="w-full"
-                  >
-                    {cancelLabel}
-                  </Button>
-                ),
-                waiting: (
-                  <Button
-                    size={"md"}
-                    variant={"outlinedSecondary"}
-                    className="w-full"
-                  >
-                    <LoadingSpinner className="w-4 h-4 animate-spin" />
-                  </Button>
-                ),
-              }}
-              onClick={async () => {
-                await onPromiseCancel();
-                setIsOpen?.(false);
-                return false;
-              }}
-            />
-          )}
-          {!onPromiseConfirm && (
-            <Button
-              size={"md"}
-              variant={"solidPrimary"}
-              className="w-full"
-              onClick={() => {
-                onConfirm?.();
-                setIsOpen?.(false);
-              }}
-            >
-              {confirmLabel}
-            </Button>
-          )}
-          {onPromiseConfirm && (
-            <WaitingButton
-              className="w-full"
-              render={{
-                normal: (
-                  <Button
-                    size={"md"}
-                    variant={"solidPrimary"}
-                    className="w-full"
-                  >
-                    {confirmLabel}
-                  </Button>
-                ),
-                waiting: (
-                  <Button
-                    size={"md"}
-                    variant={"solidPrimary"}
-                    className="w-full"
-                  >
-                    <LoadingSpinner className="w-4 h-4 animate-spin" />
-                  </Button>
-                ),
-              }}
-              onClick={async () => {
-                await onPromiseConfirm();
-                setIsOpen?.(false);
-                return false;
-              }}
-            />
-          )}
-        </div>
+        <DialogButtons
+          cancelLabel={cancelLabel}
+          confirmLabel={confirmLabel}
+          onCancel={onCancel}
+          onConfirm={onConfirm}
+          onPromiseCancel={onPromiseCancel}
+          onPromiseConfirm={onPromiseConfirm}
+          setIsOpen={setIsOpen}
+        />
       </DialogPanel>
     </HeadlessUiDialog>
+  );
+}
+
+// DialogButtons.tsx
+interface DialogButtonsProps {
+  cancelLabel: string;
+  confirmLabel: string;
+  onCancel?: (arg?: any) => void;
+  onConfirm?: (arg?: any) => void;
+  onPromiseCancel?: (arg?: any) => Promise<boolean>;
+  onPromiseConfirm?: (arg?: any) => Promise<boolean>;
+  setIsOpen?: (arg: boolean) => void;
+}
+
+function DialogButtons({
+  cancelLabel,
+  confirmLabel,
+  onCancel,
+  onConfirm,
+  onPromiseCancel,
+  onPromiseConfirm,
+  setIsOpen,
+}: DialogButtonsProps) {
+  return (
+    <div className="flex gap-2">
+      {!onPromiseCancel && (
+        <Button
+          size={"md"}
+          variant={"outlinedSecondary"}
+          className="w-full"
+          onClick={() => {
+            onCancel?.();
+            setIsOpen?.(false);
+          }}
+        >
+          {cancelLabel}
+        </Button>
+      )}
+      {onPromiseCancel && (
+        <WaitingButton
+          className="w-full"
+          render={{
+            normal: (
+              <Button
+                size={"md"}
+                variant={"outlinedSecondary"}
+                className="w-full"
+              >
+                {cancelLabel}
+              </Button>
+            ),
+            waiting: (
+              <Button
+                size={"md"}
+                variant={"outlinedSecondary"}
+                className="w-full"
+              >
+                <LoadingSpinner className="w-4 h-4 animate-spin" />
+              </Button>
+            ),
+          }}
+          onClick={async () => {
+            await onPromiseCancel();
+            setIsOpen?.(false);
+            return false;
+          }}
+        />
+      )}
+      {!onPromiseConfirm && (
+        <Button
+          size={"md"}
+          variant={"solidPrimary"}
+          className="w-full"
+          onClick={() => {
+            onConfirm?.();
+            setIsOpen?.(false);
+          }}
+        >
+          {confirmLabel}
+        </Button>
+      )}
+      {onPromiseConfirm && (
+        <WaitingButton
+          className="w-full"
+          render={{
+            normal: (
+              <Button size={"md"} variant={"solidPrimary"} className="w-full">
+                {confirmLabel}
+              </Button>
+            ),
+            waiting: (
+              <Button size={"md"} variant={"solidPrimary"} className="w-full">
+                <LoadingSpinner className="w-4 h-4 animate-spin" />
+              </Button>
+            ),
+          }}
+          onClick={async () => {
+            await onPromiseConfirm();
+            setIsOpen?.(false);
+            return false;
+          }}
+        />
+      )}
+    </div>
   );
 }
