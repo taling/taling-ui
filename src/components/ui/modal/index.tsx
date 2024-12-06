b1484bb31df603ead71bf17a9769667490ae7262
Yunhee Won <yunhee.won.swe@gmail.com>
2024-12-06 18:27:37 +0900

    Feat: [TONE-1240] Modal Panel에 optional한 maxWidth값을 추가했습ㄴ니다

--- src/components/ui/modal/index.tsx
+++ src/components/ui/modal/index.tsx
@@ -6,6 +6,8 @@ import {
 import { classNames } from "@taling-ui/util/tailwind-util/class-names";
 import ModalButtons from "./modal-buttons";
 
+type MaxWidthOption = "sm" | "md" | "lg" | "xl" | "2xl" | "full" | null;
+
 interface ModalProps {
   title?: string;
   subTitle?: string;
@@ -17,6 +19,7 @@ interface ModalProps {
   modalType?: ModalType;
   leftIcon?: React.ReactNode;
   rightIcon?: React.ReactNode;
+  maxWidth?: MaxWidthOption;
   onCancel?: (arg?: any) => void;
   onConfirm?: (arg?: any) => void;
   onPromiseCancel?: (arg?: any) => Promise<boolean>;
@@ -31,6 +34,25 @@ const modalType = {
 };
 export type ModalType = keyof typeof modalType;
 
+const getMaxWidthClass = (maxWidth: MaxWidthOption): string => {
+  switch (maxWidth) {
+    case "sm":
+      return "max-w-sm";
+    case "md":
+      return "max-w-md";
+    case "lg":
+      return "max-w-lg";
+    case "xl":
+      return "max-w-xl";
+    case "2xl":
+      return "max-w-2xl";
+    case "full":
+      return "max-w-full";
+    default:
+      return "";
+  }
+};
+
 export default function Modal({
   title,
   subTitle,
@@ -42,6 +64,7 @@ export default function Modal({
   isBackDropClickable = true,
   isOpen,
   modalType = "twoButton",
+  maxWidth = null,
   onPromiseCancel,
   onPromiseConfirm,
   onCancel,
@@ -56,12 +79,19 @@ export default function Modal({
       }}
       transition
       className={classNames(
-        "z-[999] fixed inset-0 flex w-screen items-center justify-center bg-taling-gray-900/70",
+        `fixed inset-0 z-[999] flex w-screen items-center justify-center
+        bg-taling-gray-900/70`,
         "transition duration-300 ease-out",
         "data-[closed]:opacity-0",
       )}
     >
-      <ModalPanel className="flex flex-col gap-5 min-w-[18.4375rem] bg-taling-white px-8 py-7 rounded-2xl shadow-strong">
+      <ModalPanel
+        className={classNames(
+          `flex min-w-[18.4375rem] flex-col gap-5 rounded-2xl bg-taling-white px-8 py-7
+          shadow-strong`,
+          getMaxWidthClass(maxWidth),
+        )}
+      >
         <ModalTitle className="flex flex-col gap-1">
           <span className="text-heading3-semibold text-neutral">{title}</span>
           <span className="text-label1reading-regular text-neutral">
