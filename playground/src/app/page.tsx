f70eef2e6d708187af41077f61073e086f34fdc7
Kyusik Jeong <starplanter93@gmail.com>
2024-12-18 16:07:26 +0900

    feat: [TONE-1224] vite 날리고 next로 playground 재구축

---
+++ playground/src/app/page.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import Component from "./component";
+
+export default function Home() {
+  return <Component />;
+}
--- preset.config.js
+++ preset.config.js
@@ -2,8 +2,5 @@ const preset = require("./lib/tailwind/preset");
 
 module.exports = {
   ...preset,
-  content: [
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./playground/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/components/**/*.{js,ts,jsx,tsx,mdx}"],
 };
